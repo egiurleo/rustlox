3565a7b8e09fab251e3e7a5d3cbabff6e0bddf4a
Emily Samp <emily.samp@icloud.com>
2025-10-20 15:05:48 -0500

    Chapter 15.2
    Add a stack implementation in the VM, as well as debugging capability.

--- src/vm.rs
+++ src/vm.rs
@@ -6,16 +6,30 @@ use std::io::Write;
 const DEBUG_TRACE: bool =
   option_env!("DEBUG_TRACE_EXECUTION").is_some();
 
+const STACK_MAX: usize = 256;
+
 pub enum InterpretResult {
   InterpretOk = 0,
   // InterpretCompileError = 1,
   // InterpretRuntimeError = 2,
 }
 
-#[derive(Default)]
 pub struct VM {
   chunk: Chunk,
   ip: u8,
+  stack: [Value; STACK_MAX],
+  stack_top: usize,
+}
+
+impl Default for VM {
+  fn default() -> Self {
+    VM {
+      chunk: Chunk::default(),
+      ip: 0,
+      stack: [0.0; STACK_MAX],
+      stack_top: 0,
+    }
+  }
 }
 
 impl VM {
@@ -30,19 +44,41 @@ impl VM {
     self.run(writer)
   }
 
+  pub fn reset_stack(&mut self) {
+    self.stack_top = 0;
+  }
+
+  pub fn push(&mut self, value: Value) {
+    self.stack[self.stack_top] = value;
+    self.stack_top += 1;
+  }
+
+  pub fn pop(&mut self) -> Value {
+    self.stack_top -= 1;
+    *self.stack.get(self.stack_top).expect("Stack index out of bounds")
+  }
+
   fn run<W: Write>(&mut self, writer: &mut W) -> InterpretResult {
     let mut instruction: u8;
 
     loop {
       if DEBUG_TRACE {
+        write!(writer, "          ").unwrap();
+        for i in 0..self.stack_top {
+          let value = self.stack.get(i).expect("Stack index out of bounds");
+          write!(writer, "[ {} ]", value).unwrap();
+        }
+        write!(writer, "\n").unwrap();
+
         disassemble_instruction(&self.chunk, self.ip as usize, writer);
       }
 
       instruction = self.read_byte();
       if instruction == OpCode::OpConstant as u8 {
         let constant = self.read_constant();
-        writeln!(writer, "{}", constant).unwrap();
+        self.push(constant);
       } else if instruction == OpCode::OpReturn as u8 {
+        writeln!(writer, "{}", self.pop()).unwrap();
         return InterpretResult::InterpretOk;
       }
     }
